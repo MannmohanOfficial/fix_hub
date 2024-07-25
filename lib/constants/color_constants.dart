0c3a00097c09696672df49326cee005a071f84a6
Mannmohan <mann.mohan@entrixlabs.com>
2024-07-25 15:35:50 +0530

    added assets

---
+++ lib/constants/color_constants.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class FhColor {
+  static const colorYellow = Color(0xFFFFCD57);
+  static const colorLightYellow = Color(0xFFD2BC88);
+  static const colorGreen = Color(0xFF5FB7A7);
+  static const colorDarkGrey = Color(0xFF808080);
+  static const colorBlack = Color(0xFF1C1C1C);
+  static const colorGrey = Color(0xFFD9D9D9);
+}
